3411599008eaae589b6a6277b8173107644c22e8
seonmin8284 <seonmin8284@naver.com>
2025-06-10 19:26:19 +0900

    modified:   index.html 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 }));
